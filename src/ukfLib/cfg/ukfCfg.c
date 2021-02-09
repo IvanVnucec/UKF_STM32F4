2bf8519bb89ef2f110e71a6fed3783d69e05a112
IvanVnucec <vnucec.ivan@gmail.com>
2021-02-09 21:32:19 +0100

    .

--- src/ukfLib/cfg/ukfCfg.c
+++ src/ukfLib/cfg/ukfCfg.c
@@ -40,7 +40,7 @@
  *** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *** SOFTWARE.
 \******************************************************************************************************************************************************************************************************/
-#include "../cfg/ukfCfg.h"
+#include "ukfCfg.h"
 
 #include <math.h>
 
