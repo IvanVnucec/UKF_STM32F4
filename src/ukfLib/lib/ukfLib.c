064af90062634315fe056f0f39d4c7e9893b596b
IvanVnucec <vnucec.ivan@gmail.com>
2021-02-20 14:07:40 +0100

    reformatted licence notice

--- src/ukfLib/lib/ukfLib.c
+++ src/ukfLib/lib/ukfLib.c
@@ -4,27 +4,6 @@
  *** Codefile          : ukfLib.c
  *** Documentation     : https://web.statler.wvu.edu/%7Eirl/IRL_WVU_Online_UKF_Implementation_V1.0_06_28_2013.pdf
  ***
- *** MIT License
- ***
- *** Copyright (c) 2017 ivo-georgiev
- ***  
- *** Permission is hereby granted, free of charge, to any person obtaining a copy
- *** of this software and associated documentation files (the "Software"), to deal
- *** in the Software without restriction, including without limitation the rights
- *** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *** copies of the Software, and to permit persons to whom the Software is
- *** furnished to do so, subject to the following conditions:
- ***    
- *** The above copyright notice and this permission notice shall be included in all
- *** copies or substantial portions of the Software.
- ***      
- *** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *** SOFTWARE.
 \******************************************************************************************************************************************************************************************************/
 
 #include "ukfLib.h"
